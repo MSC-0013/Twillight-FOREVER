b060fdf5d624a0c8f3dfdb874939a14c77a332bb
MSC-0013 <soumyashreerout99@gmail.com>
2025-08-26 18:13:58 +0530

    Your message

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,15 +3,11 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
-  base: "./", // ensures relative paths for JS/CSS/images
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
     },
   },
-  build: {
-    outDir: "dist",   // Vercel will serve this folder
-    assetsDir: "assets",
-  },
+  
 });
